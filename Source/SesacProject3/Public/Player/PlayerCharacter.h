2ed074763637011b6f3f0ce42974b3a6ea31e270
myeonghoon <mhoo999@naver.com>
2023-12-21 20:35:27 +0900

    Player C++ 추가

---
+++ Source/SesacProject3/Public/Player/PlayerCharacter.h
@@ -0,0 +1,24 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "PlayerCharacter.generated.h"
+
+UCLASS()
+class SESACPROJECT3_API APlayerCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	APlayerCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:	
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
