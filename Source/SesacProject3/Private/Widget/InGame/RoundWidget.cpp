a4e9b2ba40fadc2efce77969bd2a974c3f0ea75e
Sho1007 <sho1007@kookmin.ac.kr>
2024-01-05 08:08:47 +0900

    240105 OSH : Implementing Round Widget
    240105 OSH : Implementing Round Widget

--- Source/SesacProject3/Private/Widget/InGame/RoundWidget.cpp
+++ Source/SesacProject3/Private/Widget/InGame/RoundWidget.cpp
@@ -2,4 +2,33 @@
 
 
 #include "Widget/InGame/RoundWidget.h"
+#include <Components/Image.h>
 
+void URoundWidget::HideCount()
+{
+	CountOpacity -= GetWorld()->GetDeltaSeconds();
+
+	if (CountOpacity <= 0.0f)
+	{
+		FTimerManager& TimerManager = GetWorld()->GetTimerManager();
+		TimerManager.ClearTimer(CountHideTimer);
+		Img_Count->SetVisibility(ESlateVisibility::Collapsed);
+	}
+	else
+	{
+		Img_Count->SetOpacity(CountOpacity);
+	}
+}
+
+void URoundWidget::SetCount(int32 CurrentCount)
+{
+	FTimerManager& TimerManager = GetWorld()->GetTimerManager();
+	if (CountHideTimer.IsValid())
+	{
+		TimerManager.ClearTimer(CountHideTimer);
+	}
+	Img_Count->SetVisibility(ESlateVisibility::HitTestInvisible);
+	Img_Count->SetBrushFromTexture(CountTextureArray[CurrentCount - 1]);
+
+	TimerManager.SetTimer(CountHideTimer, this, &URoundWidget::HideCount, 0.1f, true);
+}
